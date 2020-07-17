5c18860156daed04f58b1583a3323f20a96a4aaf
Sean Ransonette <sransonette@gmail.com>
2020-07-17 02:15:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,15 +42,13 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect do |word, index|
   if index != 1 
     word = word + "s" 
   else
     word = word
   end
 end
-
-
 
 
 
